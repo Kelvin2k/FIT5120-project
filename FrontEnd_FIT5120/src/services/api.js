4b2e4578b96b50df7b843db6f4f2dfba7a414e4b
azha0089 <azha0089@student.monash.edu>
2025-10-13 00:17:57 +1100

    feat: update local version with new crime map filters

--- FrontEnd_FIT5120/src/services/api.js
+++ FrontEnd_FIT5120/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:5566/api', // Change this to your backend URL
-   baseURL: 'https://ankanglife.social/api', // Change this to your backend URL
+   baseURL: 'http://54.252.184.10:5566/api', // Change this to your backend URL
   timeout: 10000, // 10 seconds timeout
   headers: {
     'Content-Type': 'application/json',
